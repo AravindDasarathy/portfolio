3ffa36f8c81e46f6e6048894ae7751cf8ec920ee
aravind-dasarat <aravind.dasarat@gmail.com>
2023-05-11 12:48:26 -0400

    adding skill section

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { About, Project, Resume } from "@/components";
+import { About, Project, Resume, Skill } from "@/components";
 
 export default function Home() {
   return (
@@ -6,6 +6,7 @@ export default function Home() {
       <About />
       <Project />
       <Resume />
+      <Skill />
     </>
   );
 }
