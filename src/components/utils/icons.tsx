821fefc46995669c53b46054c084e8ae071f5d79
aravind-dasarat <aravind.dasarat@gmail.com>
2023-05-11 09:53:58 -0400

    coding carousel and arrow icons

---
+++ src/components/utils/icons.tsx
@@ -0,0 +1,20 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faAnglesRight, faAnglesLeft} from "@fortawesome/free-solid-svg-icons";
+
+export function LeftArrow() {
+  return (
+    <FontAwesomeIcon
+      icon={faAnglesLeft}
+      className="fa-duotone fa-angles-right fa-beat-fade fa-2xl arrow-icons"
+    />
+  );
+}
+
+export function RightArrow() {
+  return (
+    <FontAwesomeIcon
+      icon={faAnglesRight}
+      className="fa-duotone fa-angles-left fa-beat-fade fa-2xl arrow-icons"
+    />
+  );
+}
